6f6d0c10c3af74003f09daeb9e3dbe8b436eae56
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2019-01-25 14:19:24 +0100

    Add: setup Prometheus metrics
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- cnapps/application.py
+++ cnapps/application.py
@@ -13,12 +13,14 @@
 # limitations under the License.
 
 import logging
+import time
 
 import sanic
 import sanic_openapi
 
 from cnapps.api import health as health
 from cnapps.api import version as version
+from cnapps.middleware.metrics import prometheus
 from cnapps import version as app_version
 
 
@@ -33,16 +35,22 @@ def creates_app():
     """
 
     LOGGER.info("Create application %s", app_version.RELEASE)
-    app = sanic.Sanic()
+    app = sanic.Sanic(__name__)
     app.static('/static', './static')
-    setup_openapi(app)
     app.blueprint(version.bp)
     app.blueprint(health.bp)
+    setup_openapi(app)
+    setup_metrics(app)
 
     return app
 
 
 def setup_openapi(app):
+    """Setup OpenAPI.
+
+    Args:
+        app [sanic.Sanic]: the main application
+    """
     app.blueprint(sanic_openapi.openapi_blueprint)
     app.blueprint(sanic_openapi.swagger_blueprint)
     app.config.API_VERSION = app_version.RELEASE
@@ -51,3 +59,27 @@ def setup_openapi(app):
     app.config.API_TERMS_OF_SERVICE = 'Use with caution!'
     app.config.API_PRODUCES_CONTENT_TYPES = ['application/json']
     app.config.API_CONTACT_EMAIL = 'nicolas.lamirault@gmail.com'
+
+
+def setup_metrics(app):
+    """Setup metrics for Prometheus.
+
+    Args:
+        app [sanic.Sanic]: the main application
+    """
+
+    LOGGER.debug("Setup Prometheus metrics")
+    app.blueprint(prometheus.REST)
+
+    @app.middleware('request')
+    async def add_key(request):
+        LOGGER.debug("Metric for request %s", request)
+        request['__START_TIME__'] = time.time()
+
+    @app.middleware('response')
+    async def custom_banner(request, response):
+        LOGGER.debug("Metric for response %s", request)
+        lat = time.time() - request['__START_TIME__']
+        prometheus.REQUEST_LATENCY.labels(request.method, request.path).observe(lat)
+        prometheus.REQUEST_COUNT.labels(request.method, request.path, response.status).inc()
+
